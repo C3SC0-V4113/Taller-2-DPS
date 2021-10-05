d4dc8c24e1576ce34c3b9700cbf52df7ddd6ee94
Francisco Valle <vallecesco@gmail.com>
2021-10-05 00:09:28 -0600

    Empezando el carrusel

--- video/src/Screens/peliculas.js
+++ video/src/Screens/peliculas.js
@@ -1,18 +1,102 @@
-import React from 'react';
-import {View, Text, StyleSheet} from 'react-native';
+import React, {useState} from 'react';
+import {
+  View,
+  StyleSheet,
+  Image,
+  Text,
+  ScrollView,
+  Modal,
+  Button,
+  TouchableHighlight,
+} from 'react-native';
 
 export default function Peliculas() {
   return (
-    <View>
-      <Text style={styles.titleText}>En Peliculas</Text>
+    <View style={styles.contenedor}>
+      <Text style={styles.titulo}>Que hacer en El Salvador</Text>
+      <ScrollView horizontal>
+        <View>
+          <TouchableHighlight>
+            <Image
+              style={styles.ciudad}
+              source={{uri:'https://cinescopia.com/wp-content/uploads/2016/01/hateful-eight-banner-what.jpg'}}
+            />
+          </TouchableHighlight>
+        </View>
+        <View>
+          <Image
+            style={styles.ciudad}
+            source={{uri:'https://cinescopia.com/wp-content/uploads/2016/01/hateful-eight-banner-what.jpg'}}
+          />
+        </View>
+        <View>
+          <Image
+            style={styles.ciudad}
+            source={{uri:'https://cinescopia.com/wp-content/uploads/2016/01/hateful-eight-banner-what.jpg'}}
+          />
+        </View>
+        <View>
+          <Image
+            style={styles.ciudad}
+            source={{uri:'https://cinescopia.com/wp-content/uploads/2016/01/hateful-eight-banner-what.jpg'}}
+          />
+        </View>
+        <View>
+          <Image
+            style={styles.ciudad}
+            source={{uri:'https://cinescopia.com/wp-content/uploads/2016/01/hateful-eight-banner-what.jpg'}}
+          />
+        </View>
+      </ScrollView>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  titleText: {
-    color: 'black',
-    fontSize: 20,
+  banner: {
+    height: 250,
+    flex: 1,
+  },
+  titulo: {
     fontWeight: 'bold',
+    fontSize: 24,
+    marginVertical: 10,
+  },
+  contenedor: {
+    marginHorizontal: 10,
+  },
+  ciudad: {
+    width: 250,
+    height: 300,
+    marginRight: 10,
+  },
+  mejores: {
+    width: '100%',
+    height: 200,
+    marginVertical: 5,
+  },
+  listadoItem: {
+    flexBasis: '49%',
+  },
+  listado: {
+    flexDirection: 'row',
+    flexWrap: 'wrap',
+    justifyContent: 'space-between',
+  },
+  vistaModal: {
+    backgroundColor: '#000000aa',
+    flex: 1,
+  },
+  Modal: {
+    backgroundColor: '#fff',
+    margin: 50,
+    padding: 40,
+    borderRadius: 10,
+    flex: 1,
+  },
+  subtitulo: {
+    fontWeight: 'bold',
+    fontSize: 14,
+    justifyContent: 'center',
   },
 });
